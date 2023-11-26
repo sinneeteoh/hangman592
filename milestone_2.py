177b2fd91f2409b8da7c2fbeb323c1cfbc921474
sinneeteoh <sinneeteoh@gmail.com>
2023-11-26 18:27:11 +0000

    Task 2 and Task 3

---
+++ milestone_2.py
@@ -0,0 +1,5 @@
+# This file contains the code for the first milestone
+import random
+word_list = ["mango", "honeydew", "durian", "dragonfruit", "kiwi"]
+word = random.choice(word_list)
+print(word)
